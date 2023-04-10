58bcbdd4096678d548a31ca260cdb73cc3b833d8
zidansh <zidansh19@gmail.com>
2023-04-10 11:25:22 +0700

    array

---
+++ src/typescript-dasar/2-array.ts
@@ -0,0 +1,11 @@
+// Array type
+let array: any[] = [1, 2, 3, 'zidan'];
+
+// Array contains string
+let arrayString: string[] = ['zidan', 'syahrul'];
+
+// Array contains number
+let arrayNumber: number[] = [1, 2, 3, 4];
+
+// Array contains some type
+let arrayMix: [number, string] = [1, '1'];
